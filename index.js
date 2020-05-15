23d50c29a2abe6e007cb43d1520d57755b6759f1
Brandon Neil <brandonneil@Brandons-MacBook-Pro.local>
2020-05-15 16:50:17 -0600

    got the first three done and the console log looks good.. im going to revisit question 2 though because i feel like i need to add splice in there somewhere... it was too easy the way i did it

--- index.js
+++ index.js
@@ -208,15 +208,15 @@ const artists = [
 (1) Name of the first artist in the array
 (2) Bio of the third artist in the array */
 
-function getFirstName(artist){
-  let firstArtist = artist[0];
-  return firstArtist
-}
-console.log(getFirstName());
+console.log(artists[0].name);
+
+console.log(artists[2].bio);
 
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
+artists[8].name = 'Vincent Van Gogh';
+console.log(artists[8].name);
 
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
@@ -229,7 +229,12 @@ console.log(getFirstName());
 */
 function getArtistByIndex(id, name) {
     /* code here */
+    for (let i = 0; i < artists.length; i++){
+      if (i === id)
+    return `The artist at index ${artists[i].id} is ${artists[i].name}.`
   }
+}
+  console.log(getArtistByIndex(13, name));
   
   /**
 
